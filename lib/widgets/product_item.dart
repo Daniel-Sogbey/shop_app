4f78e027a8fd61e5cb742cd9516904c67af9d715
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-20 09:24:20 -0800

    added a way to added the provider package to perform an action on a single product like marking a product as favorite

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem({this.id, this.title, this.imageUrl});
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // ProductItem({this.id, this.title, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context)
-                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           leading: IconButton(
             color: Theme.of(context).accentColor,
-            onPressed: () {},
+            onPressed: product.toggleFavoriteStatus,
             icon: Icon(
-              Icons.favorite_border,
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
           ),
           backgroundColor: Colors.black87,
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
