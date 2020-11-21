79051f59b7d82fa99dc0b953c3924c3b47855a25
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-20 22:59:00 -0800

    made cart items dismissible

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -58,6 +58,7 @@ class CartScreen extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 return CartItem(
                   id: cart.items.values.toList()[index].id,
+                  productId: cart.items.keys.toList()[index],
                   price: cart.items.values.toList()[index].price,
                   title: cart.items.values.toList()[index].title,
                   quantity: cart.items.values.toList()[index].quantity,
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,35 +1,61 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final String title;
   final int quantity;
 
-  CartItem({this.id, this.price, this.title, this.quantity});
+  CartItem({this.id, this.productId, this.price, this.title, this.quantity});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 15.0),
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 30.0,
+    return Consumer<Cart>(
+      builder: (_, cart, child) {
+        return Dismissible(
+          key: ValueKey(id),
+          background: Container(
+            color: Theme.of(context).errorColor,
+            child: Icon(
+              Icons.delete,
+              size: 40.0,
+              color: Colors.white,
+            ),
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(right: 20.0),
+            margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 15.0),
+          ),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            cart.removeItem(productId);
+          },
+          child: Card(
+            margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 15.0),
             child: Padding(
-              padding: EdgeInsets.all(5.0),
-              child: FittedBox(
-                child: Text('\$$price'),
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: FittedBox(
+                      child: Text('\$$price'),
+                    ),
+                  ),
+                ),
+                title: Text(title),
+                subtitle: Text('Total: \$${(quantity * price)}'),
+                trailing: Text('$quantity x'),
               ),
             ),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(quantity * price)}'),
-          trailing: Text('$quantity x'),
-        ),
-      ),
+        );
+      },
     );
   }
 }
