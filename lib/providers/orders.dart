fd9216233f53dc5460537ab01c009a50e8e56c92
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-21 21:20:28 -0800

    made all the orders fields required

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -8,7 +8,12 @@ class OrderItem {
   final List<CartItem> products;
   final DateTime dateTime;
 
-  OrderItem({this.id, this.amount, this.products, this.dateTime});
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.products,
+    @required this.dateTime,
+  });
 }
 
 class Orders with ChangeNotifier {
