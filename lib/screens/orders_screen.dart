a2338bf5af645b849e67097ce50dc200782feced
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-21 22:05:45 -0800

    added a side drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/orders.dart';
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
   @override
   Widget build(BuildContext context) {
     final orders = Provider.of<Orders>(context);
@@ -14,7 +16,9 @@ class OrdersScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: orders.orderItemCount,
         itemBuilder: (cxt, index) {
-          return;
+          return OrderItem(
+            orders.orders[index],
+          );
         },
       ),
     );
