d0a5ddcc7861cc96252c5965f6a7f15bb4b606de
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-21 21:12:50 -0800

    added an orders provider

---
+++ lib/providers/orders.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+
+import 'cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({this.id, this.amount, this.products, this.dateTime});
+}
+
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+}
