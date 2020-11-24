c6b7b9d451c7ff01e047a17e8be98c061b9efdff
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-23 23:50:49 -0800

    added a dropdown in the orders screen to display orders in detail

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,31 +1,77 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text('\$${order.amount.toStringAsFixed(2)}'),
-              subtitle: Text(
-                DateFormat('dd/MM/yyyy').format(order.dateTime),
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                });
+              },
+              icon: Icon(
+                _isExpanded ? Icons.expand_less : Icons.expand_more,
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.expand_more,
+            ),
+          ),
+          if (_isExpanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 100, 180),
+              child: ListView.builder(
+                itemCount: widget.order.products.length,
+                itemBuilder: (ctx, index) => Column(
+                  children: widget.order.products
+                      .map((product) => Container(
+                            padding: EdgeInsets.all(20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  product.title,
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                Text(
+                                  '${product.quantity}x \$${product.price}',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ))
+                      .toList(),
                 ),
               ),
             ),
-          ],
-        ));
+        ],
+      ),
+    );
   }
 }
