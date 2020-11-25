186926b9cdbe9f7e49b959f1810e83e0e9a61cef
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-24 23:17:15 -0800

    added user products screen to the app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../screens/orders_screen.dart';
+import '../screens/user_products_screen.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -29,6 +30,15 @@ class AppDrawer extends StatelessWidget {
             title: Text('Payment'),
             onTap: () {
               Navigator.of(context).pushNamed(OrdersScreen.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.edit,
+            ),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context).pushNamed(UserProductScreen.routeName);
             },
           ),
         ],
