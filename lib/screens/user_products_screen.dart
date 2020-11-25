232c219b10ebd41026dbe67dce395f3d65e9dd7f
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-11-24 23:15:46 -0800

    added user products screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+
+class UserProductScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add,
+              size: 30.0,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (ctx, index) => UserProductItem(
+            title: productsData.items[index].title,
+            imageUrl: productsData.items[index].imageUrl,
+          ),
+        ),
+      ),
+    );
+  }
+}
